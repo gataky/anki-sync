2d9504b083887c1da928740ae1f039fc8da96d78
Jeff Oryen <jeffor@zillowgroup.com>
2025-07-21 15:36:00 -0700

    print formatting

--- anki_sync/core/models/deck.py
+++ anki_sync/core/models/deck.py
@@ -62,6 +62,6 @@ class Deck(genanki.Deck):
                     }
                 )
 
-                print(f"new note found {gnote.guid}: {gnote.english}")
+                print(f"        + {gnote.guid}: {gnote.english}")
 
         return rows_to_update
